44c23811e8e4921d1d8022a9f2bcf8704ac108f1
CatOfJupit3r <catofjupit3r@gmail.com>
2024-02-18 20:23:44 +0200

    .

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -1,13 +1,72 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import COURSES from "../data/courses";
-import {Link, NavLink} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
+import queryString from "query-string";
+import {Course} from "../types/course";
+
+const getSortFunc = (sort: string) => {
+    switch (sort) {
+        case "slug":
+            return (a: Course, b: Course) => (
+                a.slug.localeCompare(b.slug)
+            )
+        case "id":
+            return (a: Course, b: Course) => (
+             b.id - a.id
+            )
+        case "title":
+            return (a: Course, b: Course) => (
+                a.title.localeCompare(b.title)
+            )
+        default:
+            return (a: Course, b: Course) => (
+                a.id - b.id
+            )
+    }
+}
+
+const SUPPORTED_SORTS = ["id", "title", "slug"]
 
 const Courses = () => {
+    const location = useLocation()
+    const navigate = useNavigate()
+    const parsedQuery = queryString.parse(location.search)
+    const [sortedCourses, setSortedCourses] = useState(COURSES)
+    const [sort, setSort] = useState(parsedQuery.sort as string | "")
+
+    useEffect(() => {
+        const evalSort = () => {
+            if (SUPPORTED_SORTS.find(s => s === sort)) {
+                setSortedCourses(prevCourses => {
+                    return [...prevCourses].sort(getSortFunc(sort))
+                })
+            } else {
+                setSortedCourses(COURSES)
+                setSort("")
+                if (sort && sort !== "") {
+                    navigate(".")
+                }
+            }
+        }
+        evalSort()
+    }, [sort, navigate]);
+
+    const handleSort = (sort: string) => {
+        if (sort) {
+            navigate(`?sort=${sort}`)
+        }
+        setSort(sort)
+    }
+
     return (
         <div>
-            {COURSES.map(course => (
-                <Link to={`${course.slug}`}>
-                    <h2>{course.title}</h2>
+            <h1>Courses sorted by: {sort ? sort[0].toUpperCase() + sort.slice(1): "None"}</h1>
+            <button type={"button"} onClick={() => handleSort("id")}>Id</button>
+            <button type={"button"} onClick={() => handleSort("title")}>Title</button>
+            <button type={"button"} onClick={() => handleSort("")}>None</button>
+            {sortedCourses.map((course, index) => (
+                <Link key={index} to={`${course.slug}`}>
+                    <h3>{course.title}</h3>
                 </Link>
             ))}
         </div>
